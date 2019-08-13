f40210cb1d52c95960436d4cc9dd6fc055bbd44d
Matvey-B <matvey.bogomolov@gmail.com>
2019-08-13 11:24:46 +0700

    MessagesList: Replaced ListGroup on to combination of Cards and Rows

--- client/src/components/MessagesList.js
+++ client/src/components/MessagesList.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import ListGroup from 'react-bootstrap/ListGroup'
+import { ListGroup, Card, Row } from 'react-bootstrap'
 
 export default class MessagesList extends React.Component {
     render() {
         console.log(`rendering MessagesList, props: `, this.props)
         const items = this.props.items.map(item => (
-            <ListGroup.Item variant="light" key={item.id}>
-                {item.text}
-            </ListGroup.Item>
+            <Row key={item.id} style={{ paddingBottom: '1em' }}>
+                <Card border="light" style={{ width: '80%' }}>
+                    <Card.Header>
+                        {item.authorId} <b>{new Date(item.createdAt).toLocaleString()}</b>
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>{item.text}</Card.Text>
+                    </Card.Body>
+                </Card>{' '}
+            </Row>
         ))
         return <ListGroup>{items}</ListGroup>
     }
